068a7d7623934c55ded182a399d49b2e4fbf5641
larisagranat <larisa.granat@gmail.com>
2019-08-05 21:55:50 -0400

    address table created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :users
 # PUT EVERYTHING BETWEEN THE NAMESPACES. THIS WILL MAKE OUR URL BEGIN WITH /api/v1
 # PUT ALL CONTROLLERS IN API/V1 FOLDER
 # class Api::V1::ExampleController
