12ac8d5979413379b8d8bdea4654f51c0120c9da
YDick <ydick1998@gmail.com>
2019-08-18 12:07:03 -0400

    removed down from drop address

--- db/migrate/20190804151451_drop_addresses.rb
+++ db/migrate/20190804151451_drop_addresses.rb
@@ -3,15 +3,4 @@ class DropAddresses < ActiveRecord::Migration[5.2]
     drop_table :addresses
   end
 
-  def down
-    create_table :addresses do |t|
-      t.string :address_line1
-      t.string :address_line2
-      t.string :city
-      t.string :province
-      t.string :postal_code
-
-      t.timestamps        
-  end
-  
 end
