8071b14c0d7832deb4218c7a1f89956cf4903007
YDick <ydick1998@gmail.com>
2019-09-06 16:22:51 -0400

    update updated all Users instead of @user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,7 +35,7 @@ class Api::V1::UsersController < ApplicationController
         @user=User.find(params[:id])
         # only current user can update
         # if @user == current_user
-        User.update(user_params)
+        @user.update(user_params)
         render json:{status: 200, user: @user}
         # else render json:{status: 403}
         # end
