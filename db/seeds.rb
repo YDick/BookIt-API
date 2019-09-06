15ddc3b32022eab39112c9a89e417c2252709162
YudiR <yudrose13@gmail.com>
2019-09-05 22:40:58 -0400

    updated seeds for maps.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,15 @@
 User.delete_all
 BookClub.delete_all
 
-u1 = User.create({name: 'seed', email: 'seed@yahoo.com', password: 'password123', address: {address_line1: '301', address_line2: 'Front St W', city: 'Toronto'}})
-u2 = User.create({name: 'seed2', email: 'seed2@yahoo.com', password: 'password123', address: {address_line1: '301', address_line2: 'Front St W', city: 'Toronto'} })
+u1 = User.create({name: 'seed', email: 'seed@yahoo.com', password: 'password123', address: {address_line1: '234 Willow Ave', city: 'Toronto', province: 'Ontario', postal_code:' M4E 3K7', country: 'canada'}})
+u2 = User.create({name: 'seed2', email: 'seed2@yahoo.com', password: 'password123', address: {address_line1: '315 St Germain Ave', city: 'Toronto', province: 'Ontario', postal_code:'ON M5M 1W4', country: 'canada'} })
+u3 = User.create({name: 'seed3', email: 'seed3@yahoo.com', password: 'password123', address: {address_line1: '500 Kingston Rd', city: 'Toronto', province: 'Ontario', postal_code:'M4L 1V3', country: 'canada'} })
 
-a = BookClub.create({name: 'clubseed', address: {address_line1: '123', address_line2: 'Front St W', city: 'Toronto'}})
-b = BookClub.create({name: 'clubseed2', address: {address_line1: '160', address_line2: 'Front St W', city: 'Toronto'}})
-c = BookClub.create({name: 'clubseed3', address: {address_line1: '255', address_line2: 'Bremner Blvd', city: 'Toronto'}})
+a = BookClub.create({name: 'clubseed', description: 'We read and talk about the best books!', address: {address_line1: '234 Willow Ave', city: 'Toronto', province: 'Ontario', postal_code:' M4E 3K7', country: 'canada'} })
+b = BookClub.create({name: 'clubseed2', description: 'We read and talk about the best books!', address:  address: {address_line1: '500 Kingston Rd', city: 'Toronto', province: 'Ontario', postal_code:'M4L 1V3', country: 'canada'}})
+c = BookClub.create({name: 'clubseed3', description: 'We read and talk about the best books!', address:  address: {address_line1: '315 St Germain Ave', city: 'Toronto', province: 'Ontario', postal_code:'ON M5M 1W4', country: 'canada'}})
+d = BookClub.create({name: 'clubseed4', description: 'We read and talk about the best books!', address:  address: {address_line1: '301 Front St W', city: 'Toronto', province: 'Ontario', postal_code:'M5V 2T6', country: 'canada'}})
+ 
 
 
 a.users << u2
